2023fdd9ab4043d3ac513e008624fc61b30f44a8
sebaspapu <78824345+sebaspapu@users.noreply.github.com>
2021-07-02 23:28:16 -0500

    Segundo Commit para las demas tribunas

--- src/parcial1/Estadio.java
+++ src/parcial1/Estadio.java
@@ -154,16 +154,354 @@ public class Estadio extends VentaBoletas{
     
     public void TribunaNorte(){
         
+        int opcion;
+        int n=0;
+        int TribunaVacia[][] = new int [5][10];
+        int contador=0;
+        double persona;
+        double sumapersona =0;        
+        
+        Cliente C = new Cliente();
+        //VentaBoletas V = new VentaBoletas();
+        
+        System.out.println("\nTRIBUNA SUR (Inicialmente VACIA)!\n");
+        
+        for(int filas=0;filas<5; filas++){
+            
+            for(int columnas=0; columnas<10;columnas++){
+                
+                System.out.print(" [ "+TribunaVacia[filas][columnas]+" ] ");
+                
+            }System.out.println("");
+            
+        }
+        
+        do{
+        
+        int CantidadBoletas = Integer.parseInt(JOptionPane.showInputDialog(null,"¿ Cual es la Cantidad de Boletas que desea Comprar ?",
+               "CANTIDAD BOLETAS",JOptionPane.QUESTION_MESSAGE));
+        
+        while(CantidadBoletas > 10){
+            
+            JOptionPane.showMessageDialog(null,"¡La cantidad de Boletas ingresada es Mayor a 10!");
+            CantidadBoletas = Integer.parseInt(JOptionPane.showInputDialog(null,"¡Ingrese una cantidad de Boletas Menor a 10!",
+               "¡CANTIDAD BOLETAS MENOR A 10!",JOptionPane.INFORMATION_MESSAGE));            
+        }        
+        CantidadB = CantidadBoletas;        
+        
+//      int filaAleatoria = (int) (Math.random()*((0-4))) ;
+        int filaAlea = (int) (Math.random() * 5)+1;
+                
+        for(int i=0; i<5;i++){
+
+            for(int j=0; j<CantidadBoletas; j++){
+                
+                TribunaVacia[filaAlea][j] = 1;
+                
+            }            
+        }
+        
+        JOptionPane.showMessageDialog(null,"Usted quedara en Cualquier fila de Tribuna de acuerdo con \n"
+                + "Nuestro sistema de seleccion");
+        
+        System.out.println("\nTribuna Despues de Compradas las Boletas!\n");
+        for(int filas=0;filas<5; filas++){
+            
+            for(int columnas=0; columnas<10;columnas++){
+                
+                System.out.print(" [ "+TribunaVacia[filas][columnas]+" ] ");
+                
+            }System.out.println("");
+            
+        }
+        
+        sumaBoletas = sumaBoletas + CantidadB;        
+        
+        persona = CantidadB * 10000;
+        
+        sumapersona = sumapersona + persona;
+        
+        contador++;
+        
+            //System.out.println("Le gustaria seguir comprando Boletas en la Tribuna Sur");
+            
+            opcion = Integer.parseInt(JOptionPane.showInputDialog("Le gustaria seguir comprando Boletas en la Tribuna Sur\n"
+                    + "\n1. SI "
+                    + "\n2. NO"));
+        
+            switch(opcion){
+                
+                case 1: break;
+                case 2: n = 2;
+                break;
+            }
+            while(opcion > 2){
+                
+                JOptionPane.showMessageDialog(null,"Por favor Ingrese una opcion Valida \n '(1 o 2)'");
+                opcion = Integer.parseInt(JOptionPane.showInputDialog("Le gustaria seguir comprando Boletas en la Tribuna Sur\n"
+                    + "\n1. SI "
+                    + "\n2. NO"));                
+                
+                switch(opcion){
+                case 1: 
+                break;
+                case 2: n = 2;
+                break;
+                }
+            }            
+            
+        }while(opcion < 2 && n != 2);
+        
+        setSumaBoletas(sumaBoletas);
+        TotalPagar = sumapersona * contador;
+        TotalPagar = TotalPagar /2;
+        setTotalPagar(TotalPagar);
+        
+//        for(int i=0;i<contador;i++){
+//        System.out.println("El total a Pagar para la Persona"+C.getNombre());
+//        }
+//        for(int i=0;i<CantidadB;i++){
+
+        System.out.println("\nEl total a pagar es: "+TotalPagar);
+        JOptionPane.showMessageDialog(null,"\nEl total a pagar para la Persona" +Arrays.toString(C.getNombre())+" es: $"+TotalPagar+ " Pesos. ","TOTAL A PAGAR",
+                JOptionPane.INFORMATION_MESSAGE);
+//        }
+        
+        
         
     }
     
     public void TribunaOriente(){
         
+        int opcion;
+        int n=0;
+        int TribunaVacia[][] = new int [5][10];
+        int contador=0;
+        double persona;
+        double sumapersona =0;        
+        
+        Cliente C = new Cliente();
+        //VentaBoletas V = new VentaBoletas();
+        
+        System.out.println("\nTRIBUNA SUR (Inicialmente VACIA)!\n");
+        
+        for(int filas=0;filas<5; filas++){
+            
+            for(int columnas=0; columnas<10;columnas++){
+                
+                System.out.print(" [ "+TribunaVacia[filas][columnas]+" ] ");
+                
+            }System.out.println("");
+            
+        }
+        
+        do{
+        
+        int CantidadBoletas = Integer.parseInt(JOptionPane.showInputDialog(null,"¿ Cual es la Cantidad de Boletas que desea Comprar ?",
+               "CANTIDAD BOLETAS",JOptionPane.QUESTION_MESSAGE));
+        
+        while(CantidadBoletas > 10){
+            
+            JOptionPane.showMessageDialog(null,"¡La cantidad de Boletas ingresada es Mayor a 10!");
+            CantidadBoletas = Integer.parseInt(JOptionPane.showInputDialog(null,"¡Ingrese una cantidad de Boletas Menor a 10!",
+               "¡CANTIDAD BOLETAS MENOR A 10!",JOptionPane.INFORMATION_MESSAGE));            
+        }        
+        CantidadB = CantidadBoletas;        
+        
+//      int filaAleatoria = (int) (Math.random()*((0-4))) ;
+        int filaAlea = (int) (Math.random() * 5)+1;
+                
+        for(int i=0; i<5;i++){
+
+            for(int j=0; j<CantidadBoletas; j++){
+                
+                TribunaVacia[filaAlea][j] = 1;
+                
+            }            
+        }
+        
+        JOptionPane.showMessageDialog(null,"Usted quedara en Cualquier fila de Tribuna de acuerdo con \n"
+                + "Nuestro sistema de seleccion");
+        
+        System.out.println("\nTribuna Despues de Compradas las Boletas!\n");
+        for(int filas=0;filas<5; filas++){
+            
+            for(int columnas=0; columnas<10;columnas++){
+                
+                System.out.print(" [ "+TribunaVacia[filas][columnas]+" ] ");
+                
+            }System.out.println("");
+            
+        }
+        
+        sumaBoletas = sumaBoletas + CantidadB;        
+        
+        persona = CantidadB * 10000;
+        
+        sumapersona = sumapersona + persona;
+        
+        contador++;
+        
+            //System.out.println("Le gustaria seguir comprando Boletas en la Tribuna Sur");
+            
+            opcion = Integer.parseInt(JOptionPane.showInputDialog("Le gustaria seguir comprando Boletas en la Tribuna Sur\n"
+                    + "\n1. SI "
+                    + "\n2. NO"));
+        
+            switch(opcion){
+                
+                case 1: break;
+                case 2: n = 2;
+                break;
+            }
+            while(opcion > 2){
+                
+                JOptionPane.showMessageDialog(null,"Por favor Ingrese una opcion Valida \n '(1 o 2)'");
+                opcion = Integer.parseInt(JOptionPane.showInputDialog("Le gustaria seguir comprando Boletas en la Tribuna Sur\n"
+                    + "\n1. SI "
+                    + "\n2. NO"));                
+                
+                switch(opcion){
+                case 1: 
+                break;
+                case 2: n = 2;
+                break;
+                }
+            }            
+            
+        }while(opcion < 2 && n != 2);
+        
+        setSumaBoletas(sumaBoletas);
+        TotalPagar = sumapersona * contador;
+        TotalPagar = TotalPagar /2;
+        setTotalPagar(TotalPagar);
+        
+//        for(int i=0;i<contador;i++){
+//        System.out.println("El total a Pagar para la Persona"+C.getNombre());
+//        }
+//        for(int i=0;i<CantidadB;i++){
+
+        System.out.println("\nEl total a pagar es: "+TotalPagar);
+        JOptionPane.showMessageDialog(null,"\nEl total a pagar para la Persona" +Arrays.toString(C.getNombre())+" es: $"+TotalPagar+ " Pesos. ","TOTAL A PAGAR",
+                JOptionPane.INFORMATION_MESSAGE);
+//        }
         
     }
     
     public void TribunaOccidente(){
         
+        int opcion;
+        int n=0;
+        int TribunaVacia[][] = new int [5][10];
+        int contador=0;
+        double persona;
+        double sumapersona =0;        
+        
+        Cliente C = new Cliente();
+        //VentaBoletas V = new VentaBoletas();
+        
+        System.out.println("\nTRIBUNA SUR (Inicialmente VACIA)!\n");
+        
+        for(int filas=0;filas<5; filas++){
+            
+            for(int columnas=0; columnas<10;columnas++){
+                
+                System.out.print(" [ "+TribunaVacia[filas][columnas]+" ] ");
+                
+            }System.out.println("");
+            
+        }
+        
+        do{
+        
+        int CantidadBoletas = Integer.parseInt(JOptionPane.showInputDialog(null,"¿ Cual es la Cantidad de Boletas que desea Comprar ?",
+               "CANTIDAD BOLETAS",JOptionPane.QUESTION_MESSAGE));
+        
+        while(CantidadBoletas > 10){
+            
+            JOptionPane.showMessageDialog(null,"¡La cantidad de Boletas ingresada es Mayor a 10!");
+            CantidadBoletas = Integer.parseInt(JOptionPane.showInputDialog(null,"¡Ingrese una cantidad de Boletas Menor a 10!",
+               "¡CANTIDAD BOLETAS MENOR A 10!",JOptionPane.INFORMATION_MESSAGE));            
+        }        
+        CantidadB = CantidadBoletas;        
+        
+//      int filaAleatoria = (int) (Math.random()*((0-4))) ;
+        int filaAlea = (int) (Math.random() * 5)+1;
+                
+        for(int i=0; i<5;i++){
+
+            for(int j=0; j<CantidadBoletas; j++){
+                
+                TribunaVacia[filaAlea][j] = 1;
+                
+            }            
+        }
+        
+        JOptionPane.showMessageDialog(null,"Usted quedara en Cualquier fila de Tribuna de acuerdo con \n"
+                + "Nuestro sistema de seleccion");
+        
+        System.out.println("\nTribuna Despues de Compradas las Boletas!\n");
+        for(int filas=0;filas<5; filas++){
+            
+            for(int columnas=0; columnas<10;columnas++){
+                
+                System.out.print(" [ "+TribunaVacia[filas][columnas]+" ] ");
+                
+            }System.out.println("");
+            
+        }
+        
+        sumaBoletas = sumaBoletas + CantidadB;        
+        
+        persona = CantidadB * 10000;
+        
+        sumapersona = sumapersona + persona;
+        
+        contador++;
+        
+            //System.out.println("Le gustaria seguir comprando Boletas en la Tribuna Sur");
+            
+            opcion = Integer.parseInt(JOptionPane.showInputDialog("Le gustaria seguir comprando Boletas en la Tribuna Sur\n"
+                    + "\n1. SI "
+                    + "\n2. NO"));
+        
+            switch(opcion){
+                
+                case 1: break;
+                case 2: n = 2;
+                break;
+            }
+            while(opcion > 2){
+                
+                JOptionPane.showMessageDialog(null,"Por favor Ingrese una opcion Valida \n '(1 o 2)'");
+                opcion = Integer.parseInt(JOptionPane.showInputDialog("Le gustaria seguir comprando Boletas en la Tribuna Sur\n"
+                    + "\n1. SI "
+                    + "\n2. NO"));                
+                
+                switch(opcion){
+                case 1: 
+                break;
+                case 2: n = 2;
+                break;
+                }
+            }            
+            
+        }while(opcion < 2 && n != 2);
+        
+        setSumaBoletas(sumaBoletas);
+        TotalPagar = sumapersona * contador;
+        TotalPagar = TotalPagar /2;
+        setTotalPagar(TotalPagar);
+        
+//        for(int i=0;i<contador;i++){
+//        System.out.println("El total a Pagar para la Persona"+C.getNombre());
+//        }
+//        for(int i=0;i<CantidadB;i++){
+
+        System.out.println("\nEl total a pagar es: "+TotalPagar);
+        JOptionPane.showMessageDialog(null,"\nEl total a pagar para la Persona" +Arrays.toString(C.getNombre())+" es: $"+TotalPagar+ " Pesos. ","TOTAL A PAGAR",
+                JOptionPane.INFORMATION_MESSAGE);
+//        }
         
     }
     
